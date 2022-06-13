1c3832befc8cd4cccc480d8220440e81e01a9bd0
0WLB34R <knhrvs@gmail.com>
2022-06-12 21:05:57 -0400

    Code restructuring

--- myatt/src/app/modules/clases/clases.component.ts
+++ myatt/src/app/modules/clases/clases.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { DbhandlerService } from 'src/app/services/dbhandler/dbhandler.service';
+import { arrayUnion } from 'firebase/firestore';
 
 @Component({
   selector: 'app-clases',
@@ -8,17 +10,70 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
   styleUrls: ['./clases.component.scss']
 })
 export class ClasesComponent implements OnInit {
+  uid!: string;
+  asProffList!:any[];
+  asStudentList!:any[];
 
-  constructor(private router: Router, private fbAuth:AngularFireAuth) { }
+  @Input() className!: string;
+  @Output() classNameChange = new EventEmitter<string>();
+
+  constructor(private fbAuth: AngularFireAuth, private dbhandler: DbhandlerService, private router:Router) {
+   }
 
   ngOnInit(): void {
+    this.fbAuth.onAuthStateChanged((user:any) =>{
+      this.uid = user.uid;
+      this.fetchClasses()
+    })
   }
 
-  toClassAdmin() {
-    this.router.navigate(['classadmin']);
+  createClass(start:any, end:any){
+
+    this.dbhandler.createClass({
+      ClassName: this.className,
+      IdProff: this.uid,
+      StartDate: start,
+      EndDate: end,
+      Students: [],
+      Attendances: []
+    }).then((res: any) =>{
+      console.log(res.id)
+      console.log("Class creation was successful")
+      this.dbhandler.updateUser(this.uid, 
+        {
+          ClassesAsProff: arrayUnion({ClassId:res.id,ClassName:this.className})
+      }).then( (res2:any) => console.log("success"))
+    })
   }
 
-  onSignOut(){
-    this.fbAuth.signOut().then(res => this.router.navigate(['login']))
+  fetchClasses(){
+    this.dbhandler.getClasses(this.uid).subscribe((res:any) =>{
+      console.log(res.data().ClassesAsProff)
+      this.asProffList = res.data().ClassesAsProff
+      this.asStudentList = res.data().ClassesAsStudent
+    })
   }
+  
+  goToAttendanceProff(classId:string){
+    this.dbhandler.infoHolder = []
+    this.dbhandler.infoHolder.push(classId)
+    this.router.navigate(['attp'])
+  }
+
+  goToAttendanceStudent(classId:string){
+    this.dbhandler.infoHolder = []
+    this.dbhandler.infoHolder.push(classId)
+    this.router.navigate(['atts'])
+  }
+
+  // markAttendance(classId:string){
+  //   this.dbhandler.infoHolder = []
+  //   this.dbhandler.infoHolder.push(classId)
+  //   this.router.navigate(['atts'])
+  // }
+
+  signUp(){
+    this.router.navigate(['classsignup'])
+  }
+
 }
