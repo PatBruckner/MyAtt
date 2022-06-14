bd478c61f98f6aecd4c5b952eddb21f4d8b0a039
0WLB34R <knhrvs@gmail.com>
2022-06-13 22:32:24 -0400

    Branch Fix

--- myatt/src/app/modules/dashboard/pages/randomizer/randomizer.component.ts
+++ myatt/src/app/modules/dashboard/pages/randomizer/randomizer.component.ts
@@ -9,7 +9,45 @@ export class RandomizerComponent implements OnInit {
 
   constructor() { }
 
+  randomList:string[]=[];
+
   ngOnInit(): void {
   }
 
+   appendToArray(word:string) {
+    console.log("mandar palabra");
+    this.randomList.push(word);
+    this.updateList(this.randomList);
+    word = "";
+
+  }
+
+  updateList(wordsArr: string[]) {
+    var list= document.getElementById('list');
+    list!.innerHTML = "";
+    for (var i =0; i < wordsArr.length; i++) {
+      var word = wordsArr[i];
+      var li = document.createElement('li');
+      li.innerText = word;
+      list!.appendChild(li);
+    }
+    console.log(wordsArr)
+  }
+
+  shuffleArr(wordsArr: string[]){
+    for(var i = 0; i < wordsArr.length; i++){
+      var tempWord = wordsArr[i];
+      var randomIndex = Math.floor(Math.random() * wordsArr.length)
+      wordsArr[i] = wordsArr[randomIndex];
+      wordsArr[randomIndex] = tempWord;
+    }
+  }
+  shuffleList() {
+    this.shuffleArr(this.randomList);
+    var list = this.randomList;
+    this.updateList(list);
+  }
+  clearList(){
+
+  }
 }
